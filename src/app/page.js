3fade811b6b347fbb7c1b8526bbf7684db8591bd
Scrobwofl <scrobwofl@gmail.com>
2024-03-03 21:48:38 +0000

    styling updates

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="bg-hero-image bg-cover bg-no-repeat bg-center flex min-h-screen flex-col items-center p-24 z-0">
       <div className="bg-gray-900 p-5 rounded-xl text-center filter shadow-lg">
-        <h1 className="text-xl font-bold p-2 text-blue-500 z-10">
+        <h1 className="text-xl font-bold p-2 text-yellow-600 z-10">
           The Magical Book Emporium at the End of the Universe
         </h1>
         <br />
